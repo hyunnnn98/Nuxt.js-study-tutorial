841c7c66bd2faa152e774b5bb1e6885c7c17fa2c
SeungHyun Cho <c0305sh@naver.com>
2021-12-29 19:37:48 +0900

    Feature : 페이지 컴포넌트 빌드 확인

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <p>Layout 테스트</p>
+      <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/product/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>프로덕트 페이지</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
