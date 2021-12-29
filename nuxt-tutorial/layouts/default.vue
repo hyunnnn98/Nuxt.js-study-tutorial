defef7378852cd5682dd27b96c432f93088b364d
SeungHyun Cho <c0305sh@naver.com>
2021-12-29 20:28:41 +0900

    Update : 레이아웃 컴포넌트를 이용한 중복 요소 제거

--- nuxt-tutorial/layouts/default.vue
+++ nuxt-tutorial/layouts/default.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-      <p>Layout 테스트</p>
-      <Nuxt/>
+    <h1>{{ $route.name }} 페이지</h1>
+    <Nuxt />
   </div>
 </template>
 
 <script>
 export default {
-
+  created () {
+    console.log(this.$route);
+  },
 }
 </script>
 
 <style>
-
 </style>
