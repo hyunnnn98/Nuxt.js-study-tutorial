250e65c72b0335deab2b90bc72e3c640ee5519eb
SeungHyun Cho <c0305sh@naver.com>
2022-01-04 19:51:44 +0900

    Feature : init axios

---
+++ nuxt-frontend/pages/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>메인 페이지입니다</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async created () {
+    const response = await axios.get('http://localhost:3000/products');
+    console.log(response)
+  },
+}
+</script>
+
+<style>
+</style>
