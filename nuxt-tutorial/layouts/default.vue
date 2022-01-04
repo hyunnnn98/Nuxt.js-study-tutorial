e2d5c971ec5bf583a077cbc218540aae6a8e0735
SeungHyun Cho <c0305sh@naver.com>
2022-01-04 19:37:08 +0900

    Add : simple backend for testing

--- nuxt-tutorial/layouts/default.vue
+++ nuxt-tutorial/layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>{{ $route.name }} 페이지</h1>
+    <NuxtLink to="/">홈페이지</NuxtLink>
+    <NuxtLink to="/main">메인 페이지</NuxtLink>
+    <NuxtLink to="/product">상품 페이지</NuxtLink>
     <Nuxt />
   </div>
 </template>
@@ -8,7 +11,7 @@
 <script>
 export default {
   created () {
-    console.log(this.$route);
+    // console.log(this.$route);
   },
 }
 </script>
