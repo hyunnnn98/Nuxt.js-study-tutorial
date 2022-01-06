0b021ee62ab105f49823ce47800219acf58ee14c
SeungHyun Cho <c0305sh@naver.com>
2022-01-06 19:51:22 +0900

    Update : beforeEnter과 유사한 pages단에서만 돌아가는 asyncData

--- nuxt-frontend/pages/main.vue
+++ nuxt-frontend/pages/main.vue
@@ -11,15 +11,16 @@
 import axios from 'axios'
 
 export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    // console.log(response)
+    const products = response.data
+    return { products }
+  },
   data() {
     return {
       products: [],
     }
-  },
-  async created() {
-    const response = await axios.get('http://localhost:3000/products')
-    console.log(response)
-    this.products = response.data
   },
 }
 </script>
