2d80f604842596f6cc35259f60cbbbcab3e31381
SeungHyun Cho <c0305sh@naver.com>
2022-01-06 20:02:48 +0900

    Update : products 목록 표시 기능 구현

--- nuxt-frontend/pages/main.vue
+++ nuxt-frontend/pages/main.vue
@@ -2,7 +2,13 @@
   <div>
     <p>메인 페이지입니다</p>
     <div>
-      {{ products }}
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <img :src="product.imageUrl" :alt="product.name">
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -14,14 +20,18 @@ export default {
   async asyncData() {
     const response = await axios.get('http://localhost:3000/products')
     // console.log(response)
-    const products = response.data
+    const products = response.data.map(item => ({
+      ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`
+    }))
+    
     return { products }
   },
-  data() {
-    return {
-      products: [],
-    }
-  },
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
 }
 </script>
 
